4294e18083a10676c988ef3086eb9c04ce7aae44
Luana Faria Lane <luanasfff@gmail.com>
2019-12-05 15:50:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,12 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition  
+  attr_reader :brand 
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -11,4 +14,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+
+
 end
+
